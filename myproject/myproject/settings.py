4ec34c4caa87d858d5402417f3b0bd77b0d8d7f1
Vincent Lee <dongho.vincent.lee@gmail.com>
2023-01-18 00:19:40 +0900

    added _static_root1

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
