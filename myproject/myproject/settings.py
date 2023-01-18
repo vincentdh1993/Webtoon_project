8a87d13850899d43198c51a9c24d3e383fbe2d3c
Vincent Lee <dongho.vincent.lee@gmail.com>
2023-01-18 14:37:55 +0900

    Docker에 Gunicorn 적용하기

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -127,6 +127,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
