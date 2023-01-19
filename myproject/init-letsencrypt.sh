f1bed798e09105d32f5c8324dd1f4f43167f4fa6
Vincent Lee <dongho.vincent.lee@gmail.com>
2023-01-19 15:34:14 +0900

    HTTPS trial

--- myproject/init-letsencrypt.sh
+++ myproject/init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(webtoonbot.com www.webtoonbot.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="dongho.vincent.lee@gmail.com" # Adding a valid address is strongly recommended
+email="dongho_vincent@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
